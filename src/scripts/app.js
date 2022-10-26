9b52254b688f88abd3bf425d53681bf8be365daf
Lucas Almeida <lucasecalmeida@gmail.com>
2022-10-25 23:58:19 -0300

    :art: persisting some data

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,32 +1,88 @@
 import { today, daysToExpire } from "./helpers/content-helpers";
 
 // *** Settings fields
+// Logo
+const $logo = document.getElementById('logo')
+const $logoComponent = document.getElementById('logoComponent')
+
+$logo.addEventListener('change', () => {
+  const file = $logo.files[0]
+  
+  if (file) {
+    const filePath = URL.createObjectURL(file)
+    const fileEl = document.createElement('img')
+    fileEl.classList.add('header__logo')
+    fileEl.setAttribute('src', filePath)
+
+    $logoComponent.replaceChildren(fileEl)
+  }
+})
+
+// Logo link
+const $logoLink = document.getElementById('logoLink')
+
+const persistentLogoLink = localStorage.getItem('llnk')
+if (persistentLogoLink) {
+  const fileEl = document.createElement('img')
+  fileEl.classList.add('header__logo')
+  fileEl.setAttribute('src', persistentLogoLink)
+
+  $logoComponent.replaceChildren(fileEl)
+  $logoLink.value = persistentLogoLink
+}
+
+$logoLink.addEventListener('change', () => {
+  const fileEl = document.createElement('img')
+  fileEl.classList.add('header__logo')
+  fileEl.setAttribute('src', $logoLink.value)
+
+  $logoComponent.replaceChildren(fileEl)
+  localStorage.setItem('llnk', $logoLink.value)
+})
+
+// Footer text
+const $footerContent = document.getElementById('footerContent')
+const $footerContentField = document.getElementById('footerContentField')
+
+const persistentFooterText = localStorage.getItem('ftxt')
+if (persistentFooterText) {
+  $footerContent.textContent = persistentFooterText
+  $footerContentField.value = persistentFooterText
+}
+
+$footerContentField.addEventListener('keyup', () => {
+  $footerContent.textContent = $footerContentField.value
+})
+$footerContentField.addEventListener('change', () => {
+  localStorage.setItem('ftxt', $footerContentField.value)
+})
+
+// Client name
 const $clientName = document.getElementById('clientName')
 const $contentClientName = document.getElementById('contentClientName')
-const clientName = $clientName.value
-
 $clientName.addEventListener('keyup', () => {
   $contentClientName.textContent = $clientName.value
 })
 
-const $daysToExpire = document.getElementById('daysToExpire')
-
-
-// *** Content
+// Days for expiration
 const $today = document.getElementById('today')
+const $daysToExpire = document.getElementById('daysToExpire')
+const $expiration = document.getElementById('expiration')
 $today.textContent = today()
 
+const persistentDaysForExpiration = localStorage.getItem('edt')
+if (persistentDaysForExpiration) {
+  $expiration.textContent = `Orçamento válido até ${daysToExpire(parseInt(persistentDaysForExpiration))}`
+  $daysToExpire.value = persistentDaysForExpiration
+}
 $daysToExpire.addEventListener('keyup', () => {
   const totalDaysToExpire = $daysToExpire.value
-  const $expiration = document.getElementById('expiration')
+  localStorage.setItem('edt', totalDaysToExpire)
   if (totalDaysToExpire !== '') {
     $expiration.textContent = `Orçamento válido até ${daysToExpire(parseInt(totalDaysToExpire))}`
   }
 })
 
-// *** Actions
-const $button = document.getElementById('create')
-$button.addEventListener('click', () => window.print())
 
 // *** Add service
 const $addServiceAction = document.getElementById('addServiceAction')
@@ -53,3 +109,7 @@ $addServiceAction.addEventListener('click', () => {
   document.getElementById('addServiceDescription').value = ''
   document.getElementById('addServiceValue').value = ''
 })
+
+// *** Actions
+const $button = document.getElementById('create')
+$button.addEventListener('click', () => window.print())
